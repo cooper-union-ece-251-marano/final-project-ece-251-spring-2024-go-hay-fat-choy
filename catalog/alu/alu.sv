3e67e2d6b937fd3112814a5f60f2aafe281c55a0
unknown <zachary.hsieh@cooper.edu>
2024-05-02 21:09:47 -0400

    first attempt alu and tbalu

--- catalog/alu/alu.sv
+++ catalog/alu/alu.sv
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
 // ECE 251 Spring 2024
-// Engineer: YOUR NAMES
+// Engineer: Zachary Hsieh and Andrew Yuan
 // 
 //     Create Date: 2023-02-07
 //     Module Name: alu
@@ -20,10 +20,47 @@ module alu
     //
     // ---------------- PORT DEFINITIONS ----------------
     //
+
+    input logic clk,
+    input logic [(n-1):0], a, b,
+    input logic [2:0] op,
+    output logic [(n-1): 0] y,
+    output logic zero
 );
     //
     // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
     //
+
+    logic [(n-1):0]invertb, sum;
+
+    assign zero = (y == {n{1'b0}});
+    assign invertb = op[2] ? ~b : b;
+    assign sumSlt = a + invertb + op[2];
+
+    always @(a,b,op) begin
+        case(op)
+            3'b000: y = a & b;
+            3'b001: y = a | b;
+            3'b010: y = a + b;
+            3'b011: y = ~(a | b);
+            3'b100: y = sumSlt;
+            3'b101: begin
+                if (a[31] != b[31])
+                    if(a[31]>b[31])
+                        y = 1;
+                    else
+                        y = 0;
+                else
+                    if(a<b)
+                        y=1;
+                    else
+                        y=0;
+            end
+            3'b110: y = 0;
+        endcase
+    end
+
+
 
 endmodule
 
--- catalog/alu/tb_alu.sv
+++ catalog/alu/tb_alu.sv
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
 // ECE 251 Spring 2024
-// Engineer: YOUR NAMES
+// Engineer: Zachary Hsieh and Andrew Yuan
 // 
 //     Create Date: 2023-02-07
 //     Module Name: tb_alu
@@ -17,7 +17,116 @@
 `include "alu.sv"
 
 module tb_alu;
-    parameter n = 32;
+    parameter N = 32;
+
+    reg [N-1:0] A;
+    reg [N-1:0] B;
+    reg [2:0] OP;
+    reg CLK;
+
+    wire [N-1:0] Y;
+
+    initial begin : initialize_variable
+    A = 0;
+    B = 0;
+    end
+
+    always begin : clock
+        #5 CLK = ~CLK;
+    end
+
+
+    //apply input vectors
+    initial begin : prog_apply_stimuli
+    #0
+    #10 
+    OP = 3'b000;
+    for(int i=0; i<2**N; i++)
+        begin
+        for(int j=0; j<2**N; j++)
+            begin
+                A = i;
+                B = j;
+                #10
+                $display("A=%b B=%b OP=%b Y=%b\n", A, B, OP, Y);
+            end
+        end
+    #10 
+    OP = 3'b001;
+    for(int i=0; i<2**N; i++)
+        begin
+        for(int j=0; j<2**N; j++)
+            begin
+                A = i;
+                B = j;
+                #10
+                $display("A=%b B=%b OP=%b Y=%b\n", A, B, OP, Y);
+            end
+        end
+    OP = 3'b010;
+    for(int i=0; i<2**N; i++)
+        begin
+        for(int j=0; j<2**N; j++)
+            begin
+                A = i;
+                B = j;
+                #10
+                $display("A=%b B=%b OP=%b Y=%b\n", A, B, OP, Y);
+            end
+        end
+    OP = 3'b011;
+    for(int i=0; i<2**N; i++)
+        begin
+        for(int j=0; j<2**N; j++)
+            begin
+                A = i;
+                B = j;
+                #10
+                $display("A=%b B=%b OP=%b Y=%b\n", A, B, OP, Y);
+            end
+        end
+    OP = 3'b100;
+    for(int i=0; i<2**N; i++)
+        begin
+        for(int j=0; j<2**N; j++)
+            begin
+                A = i;
+                B = j;
+                #10
+                $display("A=%b B=%b OP=%b Y=%b\n", A, B, OP, Y);
+            end
+        end
+    OP = 3'b101;
+    for(int i=0; i<2**N; i++)
+        begin
+        for(int j=0; j<2**N; j++)
+            begin
+                A = i;
+                B = j;
+                #10
+                $display("A=%b B=%b OP=%b Y=%b\n", A, B, OP, Y);
+            end
+        end
+    OP = 3'b110;
+    for(int i=0; i<2**N; i++)
+        begin
+        for(int j=0; j<2**N; j++)
+            begin
+                A = i;
+                B = j;
+                #10
+                $display("A=%b B=%b OP=%b Y=%b\n", A, B, OP, Y);
+            end
+        end 
+    #10
+    $finish;
+end
+
+//
+// ---------------- INSTANTIATE UNIT UNDER TEST (UUT) ----------------
+//
+adder uut(.a(A), .b(B), .clk(CLK), .op(OP), .zero(ZERO), .y(Y));
+
 
 endmodule
 `endif // TB_ALU
