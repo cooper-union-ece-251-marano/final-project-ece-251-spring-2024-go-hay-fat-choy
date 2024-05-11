84cd9015e02915a898cd0ceb0433b2865aa44e4c
unknown <zachary.hsieh@cooper.edu>
2024-05-10 21:42:40 -0400

    updated datapath.sv to include jumpreg signal and a new MUX to change outputs for PC

--- catalog/datapath/datapath.sv
+++ catalog/datapath/datapath.sv
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
 // ECE 251 Spring 2024
-// Engineer: Prof Rob Marano
+// Engineer: Zachary Hsieh and Andrew Yuan
 // 
 //     Create Date: 2023-02-07
 //     Module Name: datapath
@@ -24,7 +24,7 @@
 `include "../signext/signext.sv"
 
 module datapath
-    #(parameter n = 32)(
+    #(parameter n = 32, parameter r = 5)(
     //
     // ---------------- PORT DEFINITIONS ----------------
     //
@@ -32,6 +32,7 @@ module datapath
     input  logic        memtoreg, pcsrc,
     input  logic        alusrc, regdst,
     input  logic        regwrite, jump,
+    input  logic        jumpreg,
     input  logic [2:0]  alucontrol,
     output logic        zero,
     output logic [(n-1):0] pc,
@@ -43,18 +44,19 @@ module datapath
     // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
     //
     logic [4:0]  writereg;
-    logic [(n-1):0] pcnext, pcnextbr, pcplus4, pcbranch;
+    logic [(n-1):0] pcnext, pcnextbr, pcplus4, pcbranch, finalpcnext;
     logic [(n-1):0] signimm, signimmsh;
     logic [(n-1):0] srca, srcb;
     logic [(n-1):0] result;
 
     // "next PC" logic
-    dff #(n)    pcreg(clk, reset, pcnext, pc);
+    dff #(n)    pcreg(clk, reset, finalpcnext, pc);
     adder       pcadd1(pc, 32'b100, pcplus4);
     sl2         immsh(signimm, signimmsh);
     adder       pcadd2(pcplus4, signimmsh, pcbranch);
     mux2 #(n)   pcbrmux(pcplus4, pcbranch, pcsrc, pcnextbr);
     mux2 #(n)   pcmux(pcnextbr, {pcplus4[31:28], instr[25:0], 2'b00}, jump, pcnext);
+    mux2 #(n)   pcmux2(pcnext, aluout, jumpreg, finalpcnext);
 
     // register file logic
     regfile     rf(clk, regwrite, instr[25:21], instr[20:16], writereg, result, srca, writedata);
